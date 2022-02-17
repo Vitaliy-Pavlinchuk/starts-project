51f1abc10d9574632f571d160a0fa2560179e9f6
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-02-17 22:10:39 +0200

    add

---
+++ js/modul2.js
@@ -0,0 +1,178 @@
+// 13--2
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = Object.keys(apartment);
+// const values = Object.values([]);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(values)
+
+
+
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   console.log(hexColors);
+
+//   rgbColors.push(color.rgb);
+//   console.log(rgbColors);
+// }
+
+
+
+
+
+
+////17--2
+
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+
+
+///////// 18 -- 2
+
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+// }
+
+
+// 19--2
+
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let newArrey = [];
+//     for (const product of products) {
+//     if (product[propName]) {
+//         newArrey.push(product[propName]);
+//     }
+//   }
+//   return newArrey;kivi
+
+
+////////// 20 --2
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   let newArrey = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       newArrey += product.price * product.quantity;
+//     }
+//   }
+//   return newArrey;
+// }
+
+/////// 23--2
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {
+//   yesterday: highYesterday,
+//   today: highToday,
+//   tomorrow: highTomorrow,
+//   icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+
+//////24--2
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   console.log(hex);
+//   console.log(rgb);
+
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+//////////////////////////25 --2 
+// function calculateMeanTemperature(forecast) {
+//   const {
+//     today: { low: todayLow, high: todayHigh },
+//     tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+//   } = forecast;
+
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+
+
+
+
+
--- js/test.js
+++ js/test.js
@@ -6,54 +6,93 @@ const type = age >= 18 ? 'adult' : 'child';
 console.log(type);
 
 
+let a = 10;
+let b = 2;
+total = a + b;
+console.log(total);
 
-
-function getNameLength(name) {
-  const message = `Name ${name} is  characters long`; // Change this line
-    console.log(message)
-  return message;
-}
-getNameLength("Poly")
-getNameLength("Harambe")
-getNameLength("Billy")
-getNameLength("Joe")
-
-
-function getSubstring(string, length) {
-  const substring = `${string.slice(0, length)}`; // Change this line
-
-  // Change this line
-    console.log(substring)
-    
-  return substring;
-}
-
-getSubstring('Hello world', 3);
-getSubstring('Hello world', 6);
-getSubstring('Hello world', 8);
-getSubstring('Hello world', 11);
-getSubstring('Hello world', 0);
-
-function checkPassword(password) {
-  const ADMIN_PASSWORD = "jqueryismyjam";
+function includes(array, value) {
   // Change code below this line
-
-  let message;
-
-  if (password === ADMIN_PASSWORD) {
-    message = "Welcome!";
-  } else {
-    message = "Access denied, wrong password!";
+  for (const arrays of array) {
+    if (arrays == value) {
+      return true;
+    } else {
+      return false;
+    }
   }
+}
+includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Jupiter');
+includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus');
+includes(['apple', 'plum', 'pear', 'orange'], 'plum');
+includes(['apple', 'plum', 'pear', 'orange'], 'kiwi');
+
+
+
+
+
+
+// Change code below this line
+
+// function includes(array, value) {
+//   // Change code below this line
+//   for (const arrays of array) {
+//     if (arrays == value) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+ 
+
+
+
+// function getNameLength(name) {
+//   const message = `Name ${name} is  characters long`; // Change this line
+//     console.log(message)
+//   return message;
+// }
+// getNameLength("Poly")
+// getNameLength("Harambe")
+// getNameLength("Billy")
+// getNameLength("Joe")
+
+
+// function getSubstring(string, length) {
+//   const substring = `${string.slice(0, length)}`; // Change this line
+
+//   // Change this line
+//     console.log(substring)
+    
+//   return substring;
+// }
+
+// getSubstring('Hello world', 3);
+// getSubstring('Hello world', 6);
+// getSubstring('Hello world', 8);
+// getSubstring('Hello world', 11);
+// getSubstring('Hello world', 0);
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+//   let message;
+
+//   if (password === ADMIN_PASSWORD) {
+//     message = "Welcome!";
+//   } else {
+//     message = "Access denied, wrong password!";
+//   }
 
   
-  console.log(message)
-  return message;
-  // Change code above this line
-}
-checkPassword('mangohackzor');
-checkPassword('polyhax');
-checkPassword('jqueryismyjam');
+//   console.log(message)
+//   return message;
+//   // Change code above this line
+// }
+// checkPassword('mangohackzor');
+// checkPassword('polyhax');
+// checkPassword('jqueryismyjam');
 
 // const cart = [51, 32, 3, 45, 55, 77, 1];
 // let total = 0;
@@ -91,50 +130,50 @@ checkPassword('jqueryismyjam');
 // }
 
 
-function slugify(title) {
-  // Change code below this line
+// function slugify(title) {
+//   // Change code below this line
 
-  slug = title.toUpperCase().split("").join("-");
-  return slug
-  console.log(slug);
-  // Change code above this line
-}
-slugify('Arrays for begginers');
-slugify('English for developer');
-slugify('Ten secrets of JavaScript');
-slugify('How to become a JUNIOR developer in TWO WEEKS');
+//   slug = title.toUpperCase().split("").join("-");
+//   return slug
+//   console.log(slug);
+//   // Change code above this line
+// }
+// slugify('Arrays for begginers');
+// slugify('English for developer');
+// slugify('Ten secrets of JavaScript');
+// slugify('How to become a JUNIOR developer in TWO WEEKS');
 
 
-function makeArray(firstArray, secondArray, maxLength) {
-  // Change code below this line
-  const total = firstArray.concat(secondArray);
-  if (total.length > maxLength)
+// function makeArray(firstArray, secondArray, maxLength) {
+//   // Change code below this line
+//   const total = firstArray.concat(secondArray);
+//   if (total.length > maxLength)
    
-  {
-    return total.slice(0, maxLength);
-  } else if (total.lenght < maxLength) {
-    return total.slice();
+//   {
+//     return total.slice(0, maxLength);
+//   } else if (total.lenght < maxLength) {
+//     return total.slice();
     
-  }
+//   }
 
-  return total;
+//   return total;
   
-}
+// }
 
-// Change code above this line
-makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3);
-makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4);
-makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3);
-makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2);
-makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4);
-makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0);
+// // Change code above this line
+// makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3);
+// makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4);
+// makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3);
+// makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2);
+// makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4);
+// makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0);
 
 
-a = 2;
-b = 4;
+// a = 2;
+// b = 4;
 
-result = a + b < 4 ? 'Мало' : 'Много';
-console.log(result);
+// result = a + b < 4 ? 'Мало' : 'Много';
+// console.log(result);
 
 
 // let message;
@@ -150,55 +189,55 @@ console.log(result);
 // }
 // console.log(message)
 
-login = 'Директор';
+// login = 'Директор';
 
-let message =
-  login == 'Сотрудник'
-    ? 'Привет'
-    : login == 'Директор'
-    ? 'Здравствуйте'
-    : login == ''
-    ? 'Нет логина'
-        : '';
+// let message =
+//   login == 'Сотрудник'
+//     ? 'Привет'
+//     : login == 'Директор'
+//     ? 'Здравствуйте'
+//     : login == ''
+//     ? 'Нет логина'
+//         : '';
     
-console.log(message)
+// console.log(message)
         
 
-const cart = [51, 32, 3, 45, 55, 77, 1, 3, 12, 65];
-let totall = 0;
+// const cart = [51, 32, 3, 45, 55, 77, 1, 3, 12, 65];
+// let totall = 0;
 
-for (const value of cart) {
-console.log(value);
-  if (value % 2 === 0) {
-    console.log("не чотное");
-  }
-  totall += value;
+// for (const value of cart) {
+// console.log(value);
+//   if (value % 2 === 0) {
+//     console.log("не чотное");
+//   }
+//   totall += value;
 
  
-}
-console.log(totall); 
+// }
+// console.log(totall); 
 
 
-const numbers = [51, 32, 3, 45, 55, 77, 1, 3, 12, 65];
-let total = 0;
+// const numbers = [51, 32, 3, 45, 55, 77, 1, 3, 12, 65];
+// let total = 0;
 
-for (const number of numbers) {
-  console.log(number);
-  if (number % 2 === 0) {
-    console.log('четноеfffff!!!');
-    total += number;
-  }
-}
+// for (const number of numbers) {
+//   console.log(number);
+//   if (number % 2 === 0) {
+//     console.log('четноеfffff!!!');
+//     total += number;
+//   }
+// }
 
-//2-19
+// //2-19
 
-const fruits = ['apple', 'plum', 'pear', 'orange'];
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
 
-for (let i = 0; i < fruits.length; i += 1) {
-  // Change this line
-  const fruit = fruits[i]; // Change this line
-  console.log(fruit);
-}
+// for (let i = 0; i < fruits.length; i += 1) {
+//   // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
 
 
 
@@ -236,22 +275,22 @@ for (let i = 0; i < fruits.length; i += 1) {
 // findLongestWord('Google do a roll');
 // findLongestWord('May the force be with you');
 
-2/22
+// 2/22
 
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
-  // Change code below this line
-  for (let i = min; i <= max; i++) {
-    numbers.push(i);
-    numbers.push(66);
-  }
-  console.log(numbers);
-  // Change code above this line
-  return numbers;
-}
-createArrayOfNumbers(1, 3);
-createArrayOfNumbers(14, 17);
-createArrayOfNumbers(29, 34);
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (let i = min; i <= max; i++) {
+//     numbers.push(i);
+//     numbers.push(66);
+//   }
+//   console.log(numbers);
+//   // Change code above this line
+//   return numbers;
+// }
+// createArrayOfNumbers(1, 3);
+// createArrayOfNumbers(14, 17);
+// createArrayOfNumbers(29, 34);
 
 
 
@@ -261,40 +300,59 @@ createArrayOfNumbers(29, 34);
 
  
 
-const cartss1 = [51, 32, 3, 45, 55, 77, 11];
-let totalssа = 0;
+// const cartss1 = [51, 32, 3, 45, 55, 77, 11];
+// let totalssа = 0;
 
-for (let i = 0; i < cartss1.length; i += 1){
+// for (let i = 0; i < cartss1.length; i += 1){
     
-    totalssа = totalssа + cartss1[i];
+//     totalssа = totalssа + cartss1[i];
+// }
+
+
+// console.log(totalssа)
+
+
+// const cartss = [51, 32, 3, 45, 55, 77, 1];
+// let totalss = 0;
+
+// for (const value of cartss) {
+//   totalss += value;
+// }
+// console.log('Total:', totalss)
+
+
+
+
+//  login['334'];
+
+// const findLogin = function (allogins, password) {
+//   return allogins.includes(password)
+//     ? `user ${password} find`
+//     : `user ${password} not find`;
+// }
+
+
+
+// console.log(findLogin('login', '334' ));
+// console.log(findLogin('login', '443q32'));
+// console.log(findLogin('login', '42q4q24'));
+// console.log(findLogin('login', '42q4q24'));
+
+//2-25
+function getCommonElements(array1, array2) {
+  // Change code below this line
+  let common = [];
+
+  for (let i = 0; i < array1.length; i += 1) {
+    if (array2.includes(array1[i])) {
+      common.push(array1[i]);
+    }
+  }
+  return common;
+  // Change code above this line
 }
-
-
-console.log(totalssа)
-
-
-const cartss = [51, 32, 3, 45, 55, 77, 1];
-let totalss = 0;
-
-for (const value of cartss) {
-  totalss += value;
-}
-console.log('Total:', totalss)
-
-
-
-
- login['334'];
-
-const findLogin = function (allogins, password) {
-  return allogins.includes(password)
-    ? `user ${password} find`
-    : `user ${password} not find`;
-}
-
-
-
-console.log(findLogin('login', '334' ));
-console.log(findLogin('login', '443q32'));
-console.log(findLogin('login', '42q4q24'));
-console.log(findLogin('login', '42q4q24'));
+getCommonElements([1, 2, 3], [2, 4]);
+getCommonElements([1, 2, 3], [2, 1, 17, 19]);
+getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]);
+getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]);
+getCommonElements([1, 2, 3], [10, 20, 30]);
